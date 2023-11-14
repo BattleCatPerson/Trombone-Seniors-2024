4f51b0fe1c32ffcd38de6c5ee215f02886ed1804
BattleCatPerson <93836439+BattleCatPerson@users.noreply.github.com>
2023-11-14 11:31:33 -0500

    AAAAAAAAAAAAAAAAAAAAAAA

--- Trombone Senior 2024 Minigames/Assets/Scripts/Max/MaxCharacterController.cs
+++ Trombone Senior 2024 Minigames/Assets/Scripts/Max/MaxCharacterController.cs
@@ -1,6 +1,9 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
+using Random = UnityEngine.Random;
 
 public class MaxCharacterController : MonoBehaviour
 {
@@ -8,16 +11,25 @@ public class MaxCharacterController : MonoBehaviour
     [SerializeField] Transform sprite;
     [SerializeField] Vector2 direction;
     [SerializeField] float speed;
+    [SerializeField] float increaseRate;
+    [SerializeField] float speedCap;
     void Start()
     {
         
     }
 
-    void Update()
+    void FixedUpdate()
     {
         sprite.position = rb.position;
-        sprite.right = direction;
-        if (direction != Vector2.zero) rb.velocity = direction.normalized * speed;
+        //sprite.right = direction;
+        if (direction != Vector2.zero)
+        {
+            rb.velocity = direction.normalized * speed;
+            var activeTouches = Touch.activeTouches;
+
+            if (activeTouches.Count > 0) speed += increaseRate * Time.fixedDeltaTime;
+            speed = Mathf.Clamp(speed, 0, speedCap);
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -31,10 +43,21 @@ public class MaxCharacterController : MonoBehaviour
 
             int index = ReturnClosestPoint(e, collision.GetContact(collision.contactCount - 1).point);
             int next = Mathf.Clamp(index + 1, 0, e.pointCount - 1);
-            int previous = Mathf.Clamp(index - 1, 0, e.pointCount - 1);
-            Vector2 slope = e.points[next] - e.points[previous];
-            direction = slope;
+            //int previous = Mathf.Clamp(index - 1, 0, e.pointCount - 1);
+            //Vector2 slope = e.points[next] - e.points[previous];
+            //direction = slope;
+            
+            if (next != index)
+            {
+                direction = e.points[next] - e.points[index];
+            }
+
         }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        direction = Vector2.zero;
     }
 
     public int ReturnClosestPoint(EdgeCollider2D e, Vector2 position)
