4d70897a7ba99832f5acd57be403bde8cf762ca7
BattleCatPerson <93836439+BattleCatPerson@users.noreply.github.com>
2023-12-04 11:30:25 -0500

    asdasdasdasd

--- Trombone Senior 2024 Minigames/Assets/Scripts/Max/MaxCharacterController.cs
+++ Trombone Senior 2024 Minigames/Assets/Scripts/Max/MaxCharacterController.cs
@@ -11,12 +11,8 @@ public class MaxCharacterController : MonoBehaviour
     [SerializeField] Transform sprite;
     [SerializeField] Vector2 direction;
     [SerializeField] float speed;
-    [SerializeField] float increaseRate;
-    [SerializeField] float speedCap;
     [SerializeField] float downwardForceRate;
-
     [SerializeField] bool colliding;
-    [SerializeField] float impactLimit;
     void Start()
     {
 
@@ -34,52 +30,37 @@ public class MaxCharacterController : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        float angle = Vector2.Angle(collision.GetContact(0).normal, collision.relativeVelocity);
-        if (collision.GetContact(0).normal.x < 0)
-        {
-            float sum = 0;
-            foreach (var c in collision.contacts)
-            {
-                Debug.Log(c.normalImpulse);
-                sum += c.normalImpulse;
-            }
-            if (sum / collision.contactCount / rb.mass > impactLimit)
-            {
-                Debug.Log("DIE");
-                rb.constraints = RigidbodyConstraints2D.FreezeAll;
-            }
-        }
-        sprite.up = collision.GetContact(0).normal;
-        colliding = true;
-    }
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        colliding = true;
-        sprite.up = collision.GetContact(0).normal;
-    }
+        //float angle = Vector2.Angle(collision.GetContact(0).normal, collision.relativeVelocity);
+        //if (collision.GetContact(0).normal.x < 0)
+        //{
+        //    float sum = 0;
+        //    foreach (var c in collision.contacts)
+        //    {
+        //        Debug.Log(c.normalImpulse);
+        //        sum += c.normalImpulse;
+        //    }
+        //    if (sum / collision.contactCount / rb.mass > impactLimit)
+        //    {
+        //        Debug.Log("DIE");
+        //        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        //    }
+        //}
+        //sprite.up = collision.GetContact(0).normal;
+        //colliding = true;
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        direction = Vector2.zero;
-        colliding = false;
+        if (collision.gameObject.CompareTag("Ramp")) rb.velocity = collision.GetContact(0).normal * rb.velocity.magnitude;
     }
+    //private void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    colliding = true;
+    //    sprite.up = collision.GetContact(0).normal;
+    //}
 
-    public int ReturnClosestPoint(EdgeCollider2D e, Vector2 position)
-    {
-        float minDistance = Mathf.Infinity;
-        int index = -1;
-        for (int i = 0; i < e.pointCount; i++)
-        {
-            Vector2 v = e.points[i];
-            float distance = Vector2.Distance(position, v);
-            if (distance < minDistance)
-            {
-                index = i;
-                minDistance = distance;
-            }
-        }
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    direction = Vector2.zero;
+    //    colliding = false;
+    //}
 
-        return index;
-    }
 
 }
