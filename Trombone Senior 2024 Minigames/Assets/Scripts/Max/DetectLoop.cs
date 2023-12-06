1a9aa6c9c0f184b07292a04356b942357c4edeee
BattleCatPerson <dankmarkers123@gmail.com>
2023-12-05 23:36:43 -0500

    utfgbh

--- Trombone Senior 2024 Minigames/Assets/Scripts/Max/DetectLoop.cs
+++ Trombone Senior 2024 Minigames/Assets/Scripts/Max/DetectLoop.cs
@@ -10,9 +10,26 @@ public class DetectLoop : MonoBehaviour
 {
     [SerializeField] float angle;
     [SerializeField] Vector2 norm;
+    [SerializeField] Vector2 pos;
+    [SerializeField] Vector2 relPos;
+    [SerializeField] RectTransform cursor;
+    [SerializeField] RectTransform center;
+
+
+    [Header("Loop Detection")]
+    [SerializeField] bool direction;
+    [SerializeField] float startAngle;
+    [SerializeField] float angleAccumulation;
+    [SerializeField] float timeToReset;
+    public enum Quadrant
+    {
+        I, II, III, IV
+    }
+
+    [SerializeField] Quadrant quadrant;
+
     void Start()
     {
-        
     }
 
     void Update()
@@ -23,9 +40,46 @@ public class DetectLoop : MonoBehaviour
         if (activeTouches.Count > 0)
         {
             //change from screen position to rect position (origin at center not at bottom)
-            norm = activeTouches[0].screenPosition.normalized;
-            angle = Mathf.Atan(norm.y / norm.x);
+            cursor.anchoredPosition = activeTouches[0].screenPosition;
+            pos = activeTouches[0].screenPosition;
+            relPos = cursor.anchoredPosition - center.anchoredPosition;
+            norm = relPos.normalized;
+            GetQuadrant();
+            ChangeAngleOffQuadrant();
         }
+    }
 
+    public void GetQuadrant()
+    {
+        if (relPos.y >= 0)
+        {
+            if (relPos.x >= 0) quadrant = Quadrant.I;
+            else quadrant = Quadrant.II;
+        }
+        else
+        {
+            if (relPos.x <= 0) quadrant = Quadrant.III;
+            else quadrant = Quadrant.IV;
+        }
+    }
+
+    public void ChangeAngleOffQuadrant()
+    {
+        float tan = Mathf.Atan(relPos.y / relPos.x) * 180 / MathF.PI;
+        switch (quadrant)
+        {
+            case Quadrant.I:
+                angle = tan;
+                break;
+            case Quadrant.II:
+                angle = 90 + (90 + tan);
+                break;
+            case Quadrant.III:
+                angle = 180 + tan;
+                break;
+            case Quadrant.IV:
+                angle = 270 + (90 + tan);
+                break;
+        }
     }
 }
