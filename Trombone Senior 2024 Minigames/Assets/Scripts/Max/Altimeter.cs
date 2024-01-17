f2e8c19836635fb117abe2b29796c86ad0d566b0
BattleCatPerson <93836439+BattleCatPerson@users.noreply.github.com>
2024-01-17 11:39:42 -0500

    aefgasefgaefgaegwgwergwergw3fgqeg

--- Trombone Senior 2024 Minigames/Assets/Scripts/Max/Altimeter.cs
+++ Trombone Senior 2024 Minigames/Assets/Scripts/Max/Altimeter.cs
@@ -5,9 +5,13 @@ using TMPro;
 public class Altimeter : MonoBehaviour
 {
     [SerializeField] float height;
+    [SerializeField] float maxMeterHeight;
     [SerializeField] int floorLayer;
     [SerializeField] Transform player;
     [SerializeField] TextMeshProUGUI text;
+    [SerializeField] Vector2 minAndMaxAngles;
+    [SerializeField] Vector2 currentAngle;
+    [SerializeField] Transform pointer;
     private void Start()
     {
         floorLayer = 1 << floorLayer;
@@ -18,5 +22,8 @@ public class Altimeter : MonoBehaviour
         Vector2 point = (Physics2D.Raycast(player.position, Vector2.down, Mathf.Infinity, floorLayer).point);
         height = Mathf.Floor(player.position.y - point.y);
         text.text = height.ToString();
+
+        //set rotation of the pointer based off height / maxMeterHeight. If height > maxMeterHeight, just make the pointer stay at the max angle.
+        // multiply the fraction by the different between the min and max angles and then add that value to the min value in order to get the right angle.
     }
 }
