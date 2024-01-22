ccf36fb963520ee952823877570d3a6f4cf053fd
BattleCatPerson <dankmarkers123@gmail.com>
2024-01-21 23:42:32 -0500

    asdasd

--- Trombone Senior 2024 Minigames/Assets/Scripts/Max/CollectibleManager.cs
+++ Trombone Senior 2024 Minigames/Assets/Scripts/Max/CollectibleManager.cs
@@ -4,15 +4,21 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 using TouchPhase = UnityEngine.InputSystem.TouchPhase;
-
+using Random = UnityEngine.Random;
 public class CollectibleManager : MonoBehaviour
 {
     [SerializeField] int layer;
+    [SerializeField] int floorLayer;
     [SerializeField] GameObject collectiblePrefab;
     [SerializeField] int collectibles;
+
+    [SerializeField] int minSpawn;
+    [SerializeField] int maxSpawn;
+    [SerializeField] float maxDistanceFromPoint;
     private void Start()
     {
         layer = 1 << layer;
+        floorLayer = 1 << floorLayer;
         if (!PlayerPrefs.HasKey("Max Collectibles")) PlayerPrefs.SetInt("Max Collectibles", 0);
         else collectibles = PlayerPrefs.GetInt("Max Collectibles");
     }
@@ -34,29 +40,55 @@ public class CollectibleManager : MonoBehaviour
     public void UpdateCollectibles() => PlayerPrefs.SetInt("Max Collectibles", collectibles);
     public void SpawnCollectibles(Vector2 v0, Vector2 pos, float g)
     {
-        //get number of collectibles to spawn
-        //only spawn when you reach a certain distance
-        //spawn them in a random area around player????? idk man im so tired!!!!!!!
-
-        //find the max height the player will reach then spawnt them there!
-        // vf = 0, 0 = vo - gt, solve for t, get horizontal velocity and multiply it by t, spawn coins at player position on launch plus the x and at the y value it peaks at.
-        // how to get yf??? 
-        //area under the graph is integral, so get the area under velocity. we have t, so multiply initial velocity by the calculated t to get the y???
-
         float prevVelY = -Mathf.Infinity;
         float velY = v0.y;
         float t = 0;
         float step = Time.deltaTime;
+        Vector2 currentPos = pos;
         while (true)
         {
             prevVelY = velY;
+            currentPos += new Vector2(v0.x * step, velY * step);
             velY -= g * step;
             t += step;
-            if (velY < prevVelY) break;
+            if (velY < 0)
+            {
+                Vector2 point = (Physics2D.Raycast(currentPos, Vector2.down, Mathf.Infinity, floorLayer).point);
+                float height = currentPos.y - point.y;
+                if (height > maxDistanceFromPoint) Spawn(currentPos);
+                break;
+            }
         }
-
         Debug.Log(t);
-        
-        
+    }
+
+    public void Spawn(Vector2 pos)
+    {
+        int count = Random.Range(minSpawn, maxSpawn);
+
+        Dictionary<float, float> dict = new();
+
+        for (int i = 0; i < count; i++)
+        {
+            float distance = 0;
+            float r = 0;
+            while (true)
+            {
+                distance = Random.Range(0, maxDistanceFromPoint);
+                r = (10 * Random.Range(0, 36)) * Mathf.PI / 180f;
+
+                if (!CheckInDictionary(dict, distance, r)) break;
+            }
+            
+            Vector2 dir = (new Vector2(Mathf.Cos(r), Mathf.Sin(r))).normalized;
+            Instantiate(collectiblePrefab, pos + dir * distance, transform.rotation);
+            dict.Add(distance, r);
+        }
+    }
+
+    public bool CheckInDictionary(Dictionary<float, float> d, float distance, float rotation)
+    {
+        if (d.ContainsKey(distance) && d.ContainsValue(rotation)) return true;
+        return false;
     }
 }
