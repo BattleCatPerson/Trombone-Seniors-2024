f404f7b578dc151dde5d78f7f5321422cc8153ca
BattleCatPerson <dankmarkers123@gmail.com>
2024-02-19 23:02:47 -0500

    asdasdas

--- Trombone Senior 2024 Minigames/Assets/Scripts/Max/PoliceCarFollow.cs
+++ Trombone Senior 2024 Minigames/Assets/Scripts/Max/PoliceCarFollow.cs
@@ -9,19 +9,13 @@ public class PoliceCarFollow : MonoBehaviour
     [SerializeField] float smoothTime;
     private Vector3 currentVelocity = Vector3.zero;
     [SerializeField] Vector3 offset;
-    [SerializeField] List<Vector3> positions;
-    [SerializeField] int index;
     void Start()
     {
-        positions.Add(target.position + offset);
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        positions.Add(target.position + offset);
         transform.right = (target.position - transform.position).normalized;
         transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref currentVelocity, smoothTime);
-        //transform.position = positions[index];
-        //index++;
     }
 }
