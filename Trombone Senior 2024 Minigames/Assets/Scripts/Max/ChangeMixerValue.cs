d5f3cadfcc66d0333f0655ada4dde2724f400eb4
BattleCatPerson <dankmarkers123@gmail.com>
2024-04-03 22:22:12 -0400

    sound done

--- Trombone Senior 2024 Minigames/Assets/Scripts/Max/ChangeMixerValue.cs
+++ Trombone Senior 2024 Minigames/Assets/Scripts/Max/ChangeMixerValue.cs
@@ -6,21 +6,48 @@ using UnityEngine.UI;
 
 public class ChangeMixerValue : MonoBehaviour
 {
-    [SerializeField] float maxValue;
-    [SerializeField] float minValue;
     [SerializeField] Slider soundSlider;
     [SerializeField] Slider musicSlider;
     [SerializeField] AudioMixer mixer;
+
+    const string SFX_VOLUME = "SoundVolume";
+    const string MUSIC_VOLUME = "MusicVolume";
+
     private void Start()
     {
-        
+        if (PlayerPrefs.HasKey(SFX_VOLUME))
+        {
+            float sV = PlayerPrefs.GetFloat(SFX_VOLUME);
+            soundSlider.value = Mathf.Pow(10, sV / 20f);
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(SFX_VOLUME, 0f);
+            soundSlider.value = 1f;
+        }
+        if (PlayerPrefs.HasKey(MUSIC_VOLUME))
+        {
+            float sV = PlayerPrefs.GetFloat(MUSIC_VOLUME);
+            musicSlider.value = Mathf.Pow(10, sV / 20f);
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(MUSIC_VOLUME, 0f);
+            musicSlider.value = 1f;
+        }
     }
-    void Update()
-    {
-        float sValue = soundSlider.value;
-        float mValue = musicSlider.value;
 
-        mixer.SetFloat("Sound", minValue + (maxValue - minValue) * sValue);
-        mixer.SetFloat("Music", minValue + (maxValue - minValue) * mValue);
+    public void UpdateSFXVolume()
+    {
+        float v = Mathf.Log10(soundSlider.value) * 20f;
+        mixer.SetFloat(SFX_VOLUME, v);
+        PlayerPrefs.SetFloat(SFX_VOLUME, v);
     }
+    public void UpdateMusicVolume()
+    {
+        float v = Mathf.Log10(musicSlider.value) * 20f;
+        mixer.SetFloat(MUSIC_VOLUME, v);
+        PlayerPrefs.SetFloat(MUSIC_VOLUME, v);
+    }
+
 }
