cb8218eedff84617474034e9e36e2afc497f7590
BattleCatPerson <93836439+BattleCatPerson@users.noreply.github.com>
2024-05-01 11:44:24 -0400

    dasdasdasd

--- Trombone Senior 2024 Minigames/Assets/Scripts/Max/LockOnPointerUp.cs
+++ Trombone Senior 2024 Minigames/Assets/Scripts/Max/LockOnPointerUp.cs
@@ -6,4 +6,14 @@ public class LockOnPointerUp : MonoBehaviour
 {
     [SerializeField] Slider slider;
     public void ResetValue() => slider.value = 0;
+    public void LockSlider()
+    {
+        ResetValue();
+        slider.interactable = false;
+    }
+
+    public void UnlockSlider()
+    {
+        slider.interactable = true;
+    }
 }
