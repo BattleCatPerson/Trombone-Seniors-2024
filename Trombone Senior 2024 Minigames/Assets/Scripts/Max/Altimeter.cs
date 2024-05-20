ab6c8a2a5f1d01625ea650ada632e279bc1d62ec
BattleCatPerson <93836439+BattleCatPerson@users.noreply.github.com>
2024-05-20 11:32:02 -0400

    sdfosdfiosdf

--- Trombone Senior 2024 Minigames/Assets/Scripts/Max/Altimeter.cs
+++ Trombone Senior 2024 Minigames/Assets/Scripts/Max/Altimeter.cs
@@ -12,8 +12,8 @@ public class Altimeter : MonoBehaviour
     [SerializeField] TextMeshProUGUI text;
     [SerializeField] Vector2 minAndMaxAngles;
     [SerializeField] float currentAngle;
-    [SerializeField] Transform pointer;
-
+    [SerializeField] RectTransform pointer;
+    [SerializeField] float areaWidth;
     [SerializeField] int multiplier;
 
     float min;
@@ -21,21 +21,21 @@ public class Altimeter : MonoBehaviour
     private void Start()
     {
         floorLayer = 1 << floorLayer;
-        currentAngle = minAndMaxAngles[0];
-        multiplier = minAndMaxAngles[1] > minAndMaxAngles[0] ? 1 : -1;
+        //currentAngle = minAndMaxAngles[0];
+        //multiplier = minAndMaxAngles[1] > minAndMaxAngles[0] ? 1 : -1;
 
-        if (minAndMaxAngles[0] > minAndMaxAngles[1])
-        {
-            min = minAndMaxAngles[1];
-            max = minAndMaxAngles[0];
-        }
-        else
-        {
-            max = minAndMaxAngles[1];
-            min = minAndMaxAngles[0];
-        }
+        //if (minAndMaxAngles[0] > minAndMaxAngles[1])
+        //{
+        //    min = minAndMaxAngles[1];
+        //    max = minAndMaxAngles[0];
+        //}
+        //else
+        //{
+        //    max = minAndMaxAngles[1];
+        //    min = minAndMaxAngles[0];
+        //}
     }
-    void Update()
+    void FixedUpdate()
     {
         //just raycast only to the floor at all times to get the altitude
         RaycastHit2D hit = Physics2D.Raycast(player.position, Vector2.down, Mathf.Infinity, floorLayer);
@@ -54,7 +54,9 @@ public class Altimeter : MonoBehaviour
         //set rotation of the pointer based off height / maxMeterHeight. If height > maxMeterHeight, just make the pointer stay at the max angle.
         // multiply the fraction by the different between the min and max angles and then add that value to the min value in order to get the right angle.
 
-        float delta = (height / maxMeterHeight) * Mathf.Abs(minAndMaxAngles[1] - minAndMaxAngles[0]);
-        pointer.transform.eulerAngles = Vector3.forward * Mathf.Clamp((minAndMaxAngles[0] + multiplier * delta), min, max);
+        //float delta = (height / maxMeterHeight) * Mathf.Abs(minAndMaxAngles[1] - minAndMaxAngles[0]);
+        //pointer.transform.eulerAngles = Vector3.forward * Mathf.Clamp((minAndMaxAngles[0] + multiplier * delta), min, max);
+
+        pointer.anchoredPosition = Vector3.right * Mathf.Lerp(-areaWidth, areaWidth, floatHeight / maxMeterHeight);
     }
 }
