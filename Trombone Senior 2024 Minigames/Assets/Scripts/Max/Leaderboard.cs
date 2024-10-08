9a204a741ce659d5429802d2707b2587d3956ee7
BattleCatPerson <93836439+BattleCatPerson@users.noreply.github.com>
2024-10-08 13:49:36 -0400

    leaderboard

---
+++ Trombone Senior 2024 Minigames/Assets/Scripts/Max/Leaderboard.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dan.Main;
+public class Leaderboard : MonoBehaviour
+{
+    private string publicLeaderboardKey = "58dcdfb1b2b6d521748f9bd92bcac44a1f9124b7e50fe14112cc5c404a0af988";
+    public void GetLeaderboard()
+    {
+        //LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg))
+    }
+}
